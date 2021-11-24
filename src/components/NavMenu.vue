121d7c5dfe6abacbc984c5d59fc5985e90a92062
张晨曦 <zhangchx11@chinatelecom.cn>
2021-11-24 10:06:34 +0800

    新增钉一钉功能

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -3,41 +3,82 @@
     <div ref="trigger" style="display: inline-block;">
       <slot />
     </div>
-    <div class="luban-nav-mask" :style="{'z-index': zIndex - 1 }" ref="navMask" v-show="open"></div>
-    <div class="luban-nav-panel" :style="{'z-index': zIndex, 'margin-top': offsetTop + 'px' }" ref="navMenu" :class="{'nav-open': open}">
-      <div class="nav-left" :style="{'z-index': zIndex + 1 }">
-        <div class="nav-all" :class="{'active': expandRight}" @mouseenter="showRight">
+    <div
+      class="luban-nav-mask"
+      :style="{ 'z-index': zIndex - 1 }"
+      ref="navMask"
+      v-show="open"
+    ></div>
+    <div
+      class="luban-nav-panel"
+      :style="{ 'z-index': zIndex, 'margin-top': offsetTop + 'px' }"
+      ref="navMenu"
+      :class="{ 'nav-open': open }"
+    >
+      <div class="nav-left" :style="{ 'z-index': zIndex + 1 }">
+        <div
+          class="nav-all"
+          :class="{ active: expandRight }"
+          @mouseenter="showRight"
+        >
           <i></i>
           全部产品
         </div>
         <ul class="nav-collect">
-          <li class="nav-item" v-for="fav in favoriteList" :key="'fav-' + (fav.menuApplicationId || fav.title)">
-            <span class="nav-item-title" @click="handleMenuClick(fav)">{{fav.title}}</span>
+          <li
+            class="nav-item"
+            v-for="fav in favoriteList"
+            :key="'fav-' + (fav.menuApplicationId || fav.title)"
+          >
+            <span class="nav-item-title" @click="handleMenuClick(fav)">{{
+              fav.title
+            }}</span>
+            <span
+              class="nav-item-ding"
+              @click="handleMenuCollection(fav)"
+              :class="isCollected(fav) ? 'collected' : ''"
+            ></span>
             <span class="nav-item-close" @click="removeFavorite(fav)"></span>
           </li>
         </ul>
       </div>
-      <div class="nav-right" :class="{'nav-right-open': expandRight}" :style="{'z-index': zIndex }">
-        <div class="pro-box" v-for="app in apps" :key="'nav-' + (app.id || app.title)">
-          <div class="pro-category">{{app.title}}</div>
+      <div
+        class="nav-right"
+        :class="{ 'nav-right-open': expandRight }"
+        :style="{ 'z-index': zIndex }"
+      >
+        <div
+          class="pro-box"
+          v-for="app in apps"
+          :key="'nav-' + (app.id || app.title)"
+        >
+          <div class="pro-category">{{ app.title }}</div>
           <div class="pro-list">
-            <div class="pro-item" v-for="item in app.appInstances" :key="'app-' + (item.id || item.title)">
+            <div
+              class="pro-item"
+              v-for="item in app.appInstances"
+              :key="'app-' + (item.id || item.title)"
+            >
               <div class="pro-item-inner">
-                <span class="pro-item-txt" @click="handleMenuClick(item)">{{item.title}}</span>
-                <span class="pro-item-star" :class="isFavorited(item) ? 'favorited' : ''" @click="setFavorite(item)"></span>
+                <span class="pro-item-txt" @click="handleMenuClick(item)">{{
+                  item.title
+                }}</span>
+                <span
+                  class="pro-item-star"
+                  :class="isFavorited(item) ? 'favorited' : ''"
+                  @click="setFavorite(item)"
+                ></span>
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 <script>
-
 export default {
-  name: 'VueLubanMenu',
+  name: "VueLubanMenu",
   props: {
     zIndex: {
       type: Number,
@@ -55,14 +96,20 @@ export default {
       type: Array,
       required: true,
       default: function() {
-				return []
-			}
+        return [];
+      }
     },
     favorites: {
       type: Array,
       default: function() {
-				return []
-			}
+        return [];
+      }
+    },
+    collections: {
+      type: Array,
+      default: function() {
+        return [];
+      }
     }
   },
   data() {
@@ -70,12 +117,16 @@ export default {
       open: false, // 展开菜单
       expandRight: false, // 展开右侧全部应用
       favoriteList: this.favorites,
+      collectionList: this.collections
     };
   },
   watch: {
     favorites(newValue, oldVaue) {
       // 响应prop变化, favoriteList由props的favorites维护
       this.favoriteList = newValue;
+    },
+    collections(newValue, oldValue) {
+      this.collectionList = newValue;
     }
   },
   mounted() {
@@ -90,31 +141,56 @@ export default {
     },
     setFavorite(app) {
       // favorites的menuApplicationId对应app的id
-      const fav = {...app, menuApplicationId: app.id}
+      const fav = { ...app, menuApplicationId: app.id };
       if (this.isFavorited(fav)) {
         this.removeFavorite(fav);
       } else {
         if (this.useDefaultAction) {
           this.favoriteList = this.favoriteList.concat(fav);
         }
-        this.$emit('favorite-add', fav);
+        this.$emit("favorite-add", fav);
       }
     },
     removeFavorite(fav) {
       if (this.useDefaultAction) {
-        this.favoriteList = this.favoriteList.filter(i => i.menuApplicationId !== fav.menuApplicationId);
+        this.favoriteList = this.favoriteList.filter(
+          i => i.menuApplicationId !== fav.menuApplicationId
+        );
       }
-      this.$emit('favorite-remove', fav);
+      this.$emit("favorite-remove", fav);
     },
-    isFavorited(app) {
-      return this.favoriteList.find(i => i.menuApplicationId === app.id)
+    isFavorited(fav) {
+      return this.favoriteList.find(i => i.menuApplicationId === fav.id);
     },
-    handleMenuClick(app) {
-      this.hideMenu();
-      this.$emit('menu-click', app);
+    isCollected(fav) {
+      return this.collectionList.find(i => i.collectedId === fav.id);
+    },
+    removeCollection(fav) {
+      if (this.useDefaultAction) {
+        this.collectionList = this.collectionList.filter(
+          i => i.collectedId !== fav.collectedId
+        );
+      }
+      this.$emit("collect-remove", fav);
+    },
+    handleMenuCollection(fav) {
+      // collectedId 用于标记是否被“钉钉”
+      fav = { ...fav, collectedId: fav.id };
+      if (this.isCollected(fav)) {
+        this.removeCollection(fav);
+      } else {
+        if (this.useDefaultAction) {
+          this.collectionList.push(fav);
+        }
+        this.$emit("collect-add", fav);
+      }
     },
     handleTriggerClick(e) {
       this.open = true;
+    },
+    handleMenuClick(app) {
+      this.hideMenu();
+      this.$emit("menu-click", app);
     },
     hideMenu(e) {
       this.expandRight = false;
@@ -125,24 +201,24 @@ export default {
       const navMenu = this.$refs.navMenu;
       const navMask = this.$refs.navMask;
       // append到body，组件隔离，页面中可同时存在多个导航菜单
-      document.querySelector('body').appendChild(navMask);
-      document.querySelector('body').appendChild(navMenu);
+      document.querySelector("body").appendChild(navMask);
+      document.querySelector("body").appendChild(navMenu);
 
-      reference.addEventListener('click', this.handleTriggerClick)
-      navMask.addEventListener('click', this.hideMenu)
+      reference.addEventListener("click", this.handleTriggerClick);
+      navMask.addEventListener("click", this.hideMenu);
     },
     dispose() {
       this.hideMenu();
       const reference = this.$refs.trigger;
       const navMenu = this.$refs.navMenu;
       const navMask = this.$refs.navMask;
-      reference.removeEventListener('click', this.handleTriggerClick)
-      navMask.removeEventListener('click', this.hideMenu)
+      reference.removeEventListener("click", this.handleTriggerClick);
+      navMask.removeEventListener("click", this.hideMenu);
       // remove navMenu
       navMask.parentNode.removeChild(navMask);
       navMenu.parentNode.removeChild(navMenu);
     }
-  },
+  }
 };
 </script>
 
@@ -166,7 +242,7 @@ export default {
   // 防止部分浏览器在translateX(-100%)仍可触发显示菜单
   visibility: hidden;
   transform: translateX(-100%);
-  transition: all .3s ease-in-out;
+  transition: all 0.3s ease-in-out;
   &.nav-open {
     transform: translateX(0);
     visibility: visible;
@@ -175,7 +251,7 @@ export default {
     z-index: 2001;
     position: relative;
     width: 250px;
-    background: #000A17;
+    background: #000a17;
     .nav-all {
       position: relative;
       padding: 16px;
@@ -199,13 +275,13 @@ export default {
       &.active {
         background: #061528;
         &::after {
-          content: '';
+          content: "";
           position: absolute;
           bottom: 0;
           left: 16px;
           right: 16px;
           height: 1px;
-          background: rgba(255,255,255,0.08);
+          background: rgba(255, 255, 255, 0.08);
         }
       }
     }
@@ -241,10 +317,27 @@ export default {
           background: url(../assets/icon-nav-close.png) no-repeat;
           background-size: contain;
         }
+        .nav-item-ding {
+          display: block;
+          width: 12px;
+          height: 12px;
+          opacity: 0;
+          background: url(../assets/icon-nav-ding.png) no-repeat;
+          background-size: contain;
+          margin-right: 16px;
+          &.collected {
+            opacity: 1;
+            background: url(../assets/icon-nav-din.png) no-repeat;
+            background-size: contain;
+          }
+        }
         &:hover {
           background: #061528;
           .nav-item-close {
             opacity: 1;
+          }
+          .nav-item-ding {
+            opacity: 0.45;
           }
         }
       }
@@ -260,7 +353,7 @@ export default {
     background: #061528;
     overflow: auto;
     transform: translateX(-100%);
-    transition: all .24s ease-in-out;
+    transition: all 0.24s ease-in-out;
     &.nav-right-open {
       transform: translateX(0);
     }
@@ -275,16 +368,16 @@ export default {
         text-align: left;
         color: #fff;
         padding-left: 24px;
-        border-bottom: 1px solid rgba(255,255,255,0.08);
+        border-bottom: 1px solid rgba(255, 255, 255, 0.08);
         &::before {
-          content: '';
+          content: "";
           display: block;
           position: absolute;
           top: 17px;
           left: 8px;
           width: 4px;
           height: 16px;
-          background: #2E92F7;
+          background: #2e92f7;
         }
       }
       .pro-list {
@@ -300,7 +393,7 @@ export default {
             margin: 0 24px;
             cursor: pointer;
             &:hover {
-              background: #15253A;
+              background: #15253a;
               border-radius: 4px;
               .pro-item-star {
                 opacity: 1;
@@ -329,7 +422,6 @@ export default {
               }
             }
           }
-
         }
       }
     }
